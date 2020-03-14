ae5bd9a3c4cca1674cd048fdfd2a70ce268d2638
Samwise <samuelfoster0@gmail.com>
2020-03-14 13:11:51 -0700

    Psuedo coded out the Javascript and added some radio forms to test.

---
+++ codequiz.js
@@ -0,0 +1,50 @@
+
+// Things needed for the quiz
+
+// A start button & welcome screen
+  // When the page loads
+  // The user is welcomed
+  // User is prompted to input a name
+
+  // If they have played before from that same IP address
+  // Then greet them by saved name
+  // A button to begin the quiz appears
+  
+// Timer that counts down
+  // when the button is pressed
+  // Initialize a countdown clock
+  // Counts down from 5 to 0
+  // Flashes BEGIN text
+
+// Questions that appear open the screen
+  // 5 questions apear one after the other
+  // Multiple choice answers appear
+    // Reasearch Radio
+  // Let the user click one
+  // Submit button to move on to the next question
+  
+// Question checker 
+  // Checks to see if they got the right answer
+  // IF they did
+  // Then give them 10 points
+  // Displays a big green check
+  // Else
+  // Red X on the screen
+  // decreases time from each question wrong.
+
+// Game over screen
+  // After time is up
+  // Or all questions answered
+  // Display Game Over
+  // Display right answer total
+  // Display wrong answer total
+  // Display total points
+
+// a score screen with input field to store user scores
+  // Score screen appears
+  // Prompts the user to input initials
+  // Places them on the board according to their score
+
+// Try again button
+  // A try agian button appears at the bottom of the screen
+  // Refreshes the page to start over again
